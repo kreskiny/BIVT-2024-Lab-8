5ec4049b5df81a6a284776e2a6f979a4f34649f5
eva <152019904+kreskiny@users.noreply.github.com>
2025-04-22 10:50:38 +0300

    Update Blue_2.cs

--- Blue_2.cs
+++ Blue_2.cs
@@ -3,9 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System;
-using Lab_8_1;
 
-namespace Lab_8_1
+namespace Lab_8
 {
     public class Blue_2 : Blue
     {
